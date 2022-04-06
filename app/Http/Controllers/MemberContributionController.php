dcc8523eeb6b80bd1464c949d4832fdbe897849c
Awuah Duodu Joseph <awuahduodujoseph@gmail.com>
2022-04-06 08:23:33 +0000

    First commit of LWCC CMS

---
+++ app/Http/Controllers/MemberContributionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MemberContribution;
+use Illuminate\Http\Request;
+
+class MemberContributionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MemberContribution  $memberContribution
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MemberContribution $memberContribution)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MemberContribution  $memberContribution
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MemberContribution $memberContribution)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MemberContribution  $memberContribution
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MemberContribution $memberContribution)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MemberContribution  $memberContribution
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MemberContribution $memberContribution)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/MemberInfoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MemberInfo;
+use Illuminate\Http\Request;
+
+class MemberInfoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MemberInfo  $memberInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MemberInfo $memberInfo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MemberInfo  $memberInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MemberInfo $memberInfo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MemberInfo  $memberInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MemberInfo $memberInfo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MemberInfo  $memberInfo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MemberInfo $memberInfo)
+    {
+        //
+    }
+}
